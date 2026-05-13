e0c241a37f613fc6fd7e2aff744332eef79eb071
Lorrany Fagundes <lorrany_br0927@hotmail.com>
2026-05-13 18:59:02 +0000

    viewsets, filtros e configuração do Swagger

--- eleicoes_api/eleicoes_api/urls.py
+++ eleicoes_api/eleicoes_api/urls.py
@@ -36,6 +36,6 @@ urlpatterns = [
     path('admin/', admin.site.core_admin_site if hasattr(admin.site, 'core_admin_site') else admin.site.urls),
     path('api/', include(router.urls)),
     
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-ui'),
 ]
